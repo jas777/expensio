c2817f45a0e89c4ea822d328df5e92c23b27a5df
jas777 <jas.czeszejko@wp.pl>
2022-01-10 22:55:35 +0100

    Fix backend tests

--- apps/backend/src/auth/auth.controller.spec.ts
+++ apps/backend/src/auth/auth.controller.spec.ts
@@ -1,11 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import UserEntity from '../shared/entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import constants from '../shared/auth/constants';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from '../shared/auth/LocalStrategy';
+import { JwtStrategy } from '../shared/auth/JwtStrategy';
+import { Repository } from 'typeorm';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        PassportModule,
+        JwtModule.register({
+          secret: constants.secret,
+          signOptions: {
+            expiresIn: '2h',
+          },
+        }),
+      ],
+      providers: [
+        AuthService,
+        LocalStrategy,
+        JwtStrategy,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useClass: Repository,
+        },
+      ],
       controllers: [AuthController],
     }).compile();
 
--- apps/backend/src/auth/auth.service.spec.ts
+++ apps/backend/src/auth/auth.service.spec.ts
@@ -1,12 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import UserEntity from '../shared/entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import constants from '../shared/auth/constants';
+import { LocalStrategy } from '../shared/auth/LocalStrategy';
+import { JwtStrategy } from '../shared/auth/JwtStrategy';
+import { AuthController } from './auth.controller';
+import { Repository } from 'typeorm';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [
+        PassportModule,
+        JwtModule.register({
+          secret: constants.secret,
+          signOptions: {
+            expiresIn: '2h',
+          },
+        }),
+      ],
+      providers: [
+        AuthService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useClass: Repository,
+        },
+      ],
+      controllers: [AuthController],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
